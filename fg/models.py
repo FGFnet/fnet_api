03c1aff014c263f00b0dbb04322128083e69963d
HAKSAN <cranemont@gmail.com>
2022-01-04 02:14:04 +0900

    feat: Add models

--- fg/models.py
+++ fg/models.py
@@ -1,3 +1,9 @@
 from django.db import models
+from django.core.validators import MinLengthValidator
+from django.db.models.fields import BooleanField
 
 # Create your models here.
+class FG(models.Model):
+    name = models.CharField(max_length=30)
+    student_id = models.CharField(max_length=10, validators=[MinLengthValidator(10)])
+    admin = BooleanField(default=False) # True = 운영진, False = 활동기수
--- lc/models.py
+++ lc/models.py
@@ -1,3 +1,12 @@
 from django.db import models
+from django.db.models.deletion import CASCADE
+from django.db.models.fields import CharField, DateField, IntegerField
+from django.db.models.fields.related import ForeignKey
+from fg.models import FG
 
 # Create your models here.
+class LC(models.Model):
+    fg = ForeignKey(FG, on_delete=CASCADE)
+    name = CharField(max_length=10)
+    total = IntegerField()
+    schedule = DateField(null=True)
