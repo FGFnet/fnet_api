a7c76d6c9d21b4db41721f2e9b5d360ed0bbc40d
goo314 <rn0731@naver.com>
2022-01-06 23:02:50 +0900

    fix: Add serializer in LcAPI

--- lc/views.py
+++ lc/views.py
@@ -1,10 +1,13 @@
+from datetime import date
 from django.core.checks.messages import Error
 from django.db.models.query import QuerySet
 from django.shortcuts import render
+from rest_framework import serializers
 from rest_framework.views import APIView
 from rest_framework.response import Response
 from .models import LC
-from .serializers import LCSerializer
+from .models import FG
+from .serializers import CreateLCSerializer, EditLCSerializer, LCSerializer
 
 # Create your views here.
 class LcAPI(APIView):
@@ -25,36 +28,47 @@ class LcAPI(APIView):
     
     def delete(self, request):
         lc_id = request.GET.get("id")
-        error, data = False, None
         if not lc_id:
-            data = "Invalid parameter"
-            error = True
+            return Response({"error":True, "data":"Invalid parameter"})
+
         try:
             queryset = LC.objects.get(id=lc_id)
             queryset.delete()
         except LC.DoesNotExist:
-            data = "LC does not exist"
-            error = True
-        return Response({"error":error, "data":data})
+            return Response({"error":True, "data":"LC does not exist"})
+        
+        return Response({"error":False, "data":None})
     
     def post(self, request):
         data = request.data
-        LC.objects.create(fg_id = data["fg_id"],
-                            name = data["name"],
-                            total = data["total"],
-                            schedule = None)
-        return Response({"error": False, "data":None})
+        serializer = CreateLCSerializer(data=data)
+        if not serializer.is_valid():
+            return Response({"error": True, "data":"Not valid"})
+        
+        try:
+            fg = FG.objects.get(name=data["fg"])
+        except FG.DoesNotExist:
+            return Response({"error": True, "data": "FG does not exist"})
+
+        LC.objects.create(fg=fg,
+                          name=data["name"],
+                          total=data["total"],
+                          schedule=None)
+        return Response({"error": False, "data":serializer.data})
 
     def put(self, request):
         data = request.data
-        error = False
+        serializer = EditLCSerializer(data=data)
+        if not serializer.is_valid():
+            return Response({"error": True, "data":"Not valid"})
+
         try:
             lc = LC.objects.get(id=data.pop("id"))
         except LC.DoesNotExist:
-            error = True
+            return Response({"error":True, "data":"LC does not exist"})
         
         for k, v in data.items():
             setattr(lc, k, v)
         lc.save()
 
-        return Response({"error":error, "data":None})
+        return Response({"error":False, "data":None})
