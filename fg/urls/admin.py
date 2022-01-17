c417ac438156985792d466b76ce01ca0911b2661
SOLBI1028 <hagkthfql@naver.com>
2022-01-17 17:11:50 +0900

    fix: Add trailing slash to fg/file url

--- fg/urls/admin.py
+++ fg/urls/admin.py
@@ -4,6 +4,6 @@ from ..views.admin import FGAPI, FGFileUploadAPI, searchFG
 
 urlpatterns = [
     path('fg/', FGAPI.as_view(), name="fg_admin_api"),
-    path('fg/file', FGFileUploadAPI.as_view(), name="fg_file_upload_admin_api"),
+    path('fg/file/', FGFileUploadAPI.as_view(), name="fg_file_upload_admin_api"),
     path('fg/search/', searchFG, name="search_fg_by_name"),
 ]
